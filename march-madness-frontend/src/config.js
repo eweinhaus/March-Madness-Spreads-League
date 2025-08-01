9c23a86f259a0b974ef9e27af26de9c032730b48
Ethan Weinhaus <eweinhaus@elon.edu>
2025-07-31 22:51:34 -0400

    call correct backend

--- march-madness-frontend/src/config.js
+++ march-madness-frontend/src/config.js
@@ -2,4 +2,4 @@ const isDevelopment = import.meta.env.MODE === 'development';
 
 export const API_URL = isDevelopment 
   ? 'http://localhost:8000'
-  : 'https://spreads-backend-qyw5.onrender.com'; 
+  : 'https://march-madness-backend-qyw5.onrender.com'; 
